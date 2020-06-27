3d63c200a82879e4f4b85a91e86e6b763852d1c5
Jefferson Ruiz <jefferson.ruiz2108@gmail.com>
2020-06-26 21:08:48 -0500

    Se agregan archivos base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simulacion_Comercial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
